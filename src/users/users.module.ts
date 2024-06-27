4c04bcf1c3a02fca0708cdc1989f8e7f01eaedc5
Marcin Niedbalec <marcin.niedbalec@proton.me>
2024-06-27 21:38:12 +0200

    fix: add exports DrizzleService from DrizzleModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DrizzleModule } from 'src/drizzle/drizzle.module';
-import { DrizzleService } from 'src/drizzle/drizzle.service';
 
 @Module({
   imports: [DrizzleModule],
-  providers: [UsersService, DrizzleService],
+  providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
 })
