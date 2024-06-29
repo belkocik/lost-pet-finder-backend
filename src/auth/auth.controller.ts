8a9336da973fbd39ee6c950bd6ae9bc5dcd14735
Marcin Niedbalec <marcin.niedbalec@proton.me>
2024-06-29 08:26:14 +0200

    setup auth service and controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('local/signup')
+  signupLocal() {
+    return this.authService.signupLocal();
+  }
+
+  @Post('local/signin')
+  signinLocal() {
+    return this.authService.signinLocal();
+  }
+
+  @Post('logout')
+  logout() {
+    return this.authService.logout();
+  }
+
+  @Post('refresh')
+  refreshTokens() {
+    return this.authService.refreshTokens();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
-  async validateUser(email: string, password: string) {
-    const user = await this.usersService.findOneWithEmail(email);
-  }
+  signupLocal() {}
+  signinLocal() {}
+  logout() {}
+  refreshTokens() {}
 }
